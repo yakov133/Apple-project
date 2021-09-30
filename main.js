84301574bd08dc7fad3bca9065d955e461f27012
“yakov133” <yakov133@walla.com>
2021-09-30 18:02:27 +0300

    email fixing

--- main.js
+++ main.js
@@ -11,6 +11,18 @@ function send(){
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(input.value.match(regexEmail)){
         alert("your email was updated");
+        let value = document.getElementById("email").value;
+        Email.send({
+            Host : "smtp.mailtrap.io",
+            Username : "ac91fc541ef82d",
+            Password : "5e818af118d7cb",
+            To : 'appleproject133@gmail.com',
+            From : `${value}`,
+            Subject : "Test email",
+            Body : `Add me to your Newsletter ${value}`
+        }).then(
+        message => alert(message)
+        );
         input.value = "";
     }else{
         let p = document.getElementById("wrongEmail");
